57d909119ae53f0a652caadbe2a3c6e6103f566c
Rajnish <rockstarrajnish4658@gmail.com>
2021-12-25 16:21:06 +0530

    saving to github

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ var q8= document.quiz.question8.value;
 var q9= document.quiz.question9.value;
 var q10= document.quiz.question10.value;
 var q11= document.quiz.question11.value;
+var q12= document.quiz.question12.value;
+var q13= document.quiz.question13.value;
+var q14= document.quiz.question14.value;
+var q15= document.quiz.question15.value;
+var q16= document.quiz.question16.value;
+var q17= document.quiz.question17.value;
+var q18= document.quiz.question18.value;
+var q19= document.quiz.question19.value;
+var q20= document.quiz.question20.value;
 if(q1=="Operating system") {c++}
 if(q2=="Animation/movie file") {c++}
 if(q3=="Animation/movie file") {c++}
@@ -22,5 +31,14 @@ if(q8=="Fluorescent Multilayer Disc") {c++}
 if(q9=="44.1 KHZ") {c++}
 if(q10=="Mcdonalds screensaver") {c++}
 if(q11=="Secure Socket Layer") {c++}
-document.getElementById("result").innerHTML="you get score"+ " "+c+" "+ "out of 11"
+if(q12=="Application software") {c++}
+if(q13=="Modifier") {c++}
+if(q14=="Operating system") {c++}
+if(q15=="Formatting") {c++}
+if(q16=="Spam") {c++}
+if(q17=="Horizontal market software") {c++}
+if(q18=="Portrait") {c++}
+if(q19=="All of the above") {c++}
+if(q20=="Mother board") {c++}
+document.getElementById("result").innerHTML="you get score"+ " "+c+" "+ "out of 20"
 }
